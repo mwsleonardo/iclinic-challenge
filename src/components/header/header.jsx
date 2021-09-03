304a756f4bde4ccf964e7d7b001996370e69a156
mwsleonardo <mwsleonardo@gmail.com>
2021-09-03 02:04:03 -0300

    Atualização header v2.0
    Ajustes nos textos, css e particles.

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,16 +6,16 @@ const Header = () => {
     return (
         <div className="headerWrapper">
             <div className="main-info">
-                <h1>Clínicas de Michael</h1>
+                <h1>Sua vida sempre bela</h1>
                 <Typed 
                 className="typed-text"
-                strings={[ "Profissionais de confiança", "Acessibilidade e transparência", "Atendimento personalizado", "Valorizamos sua felicidade" ]}
+                strings={[ "Compromisso", "Respeito", "Humanização", "Comprometimento", "Ética", "Responsabilidade" ]}
                 typeSpeed={40}
                 backSpeed={60}
                 loop
                 />
 
-                <a href="" className="btn-main-offer">Conheça nossas clínicas</a>
+                <a href="" className="btn-main-offer">Agende sua consulta</a>
             </div>
         </div>
     )
